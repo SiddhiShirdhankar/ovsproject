44e9f8c69da47e289969bc06093edca3ea00262c
SiddhiShirdhankar <98864931+SiddhiShirdhankar@users.noreply.github.com>
2022-02-02 11:49:45 +0530

    Add files via upload

---
+++ onlinevegetablesales/onlinevegetablesales/src/test/java/com/ovs/java/onlinevegetablesales/OnlinevegetablesalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovs.java.onlinevegetablesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinevegetablesalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
